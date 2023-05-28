08eb3be46f682f692b098ea5526fe48acb28b03d
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-28 23:36:32 +0400

    Add files via upload

---
+++ dimond pattern PoD/dimond pattern PoD/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 10; 
+        int midRow = rows / 2;
+
+        for (int i = 0; i <= midRow; i++)
+        {
+            for (int j = 0; j < rows - i; j++)
+                Console.Write(" ");
+
+            for (int k = 0; k <= i * 2; k++)
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+
+        for (int i = midRow - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < rows - i; j++)
+                Console.Write(" ");
+
+            for (int k = 0; k <= i * 2; k++)
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+    }
+}
