2ba844aef0f4a4e6e161fb34fd8d04e950d54803
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-28 23:45:24 +0400

    Add files via upload

---
+++ PoD-rotates array/PoD-rotates array/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PoD_rotates_array
+{
+   
+class Program
+        {
+            static void Main()
+            {
+                int[] myArray = { 1, 2, 3, 4, 5 };
+                int rotations = 2;
+
+                RotateArrayLeft(myArray, rotations);
+
+                Console.WriteLine("Rotated Array:");
+                foreach (int num in myArray)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Number of rotations: " + rotations);
+            }
+
+            static void RotateArrayLeft(int[] arr, int rotations)
+            {
+                int length = arr.Length;
+                rotations = rotations % length;
+
+                for (int i = 0; i < rotations; i++)
+                {
+                    int temp = arr[0];
+                    for (int j = 0; j < length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    arr[length - 1] = temp;
+                }
+            }
+        }
+
+    }
