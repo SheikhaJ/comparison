9f776c19644cad6579d0ed4c87615d01820ca42d
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-05-28 23:39:49 +0400

    Add files via upload

---
+++ repeatedValue/repeatedValue/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace repeatedValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 7, 5, 8, 2, 1 };
+            int result = notReapeted(arr);
+            Console.WriteLine(result);
+        }
+
+        static int notReapeted(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool repeated = false;
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && arr[j] == arr[i])
+                    {
+                        repeated = true;
+                        break;
+                    }
+                }
+
+                if (!repeated)
+                {
+                    return arr[i];
+                }
+            }
+
+            return 0; // if no non-repeating value is found
+        }
+    }
+}
